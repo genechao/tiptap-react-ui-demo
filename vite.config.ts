17b90d149d58d3871ec60cf9503e826314851b21
genechao <100798052+genechao@users.noreply.github.com>
2025-07-20 17:42:48 +0000

    Update vite config to build using relative base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Uses a relative base path for builds, necessary when deploying to a subdirectory.
+  base: './',
   plugins: [react(), tailwindcss()],
 })
